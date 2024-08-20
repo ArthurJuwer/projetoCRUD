bbb36d7e445a939409950d88e3218a8a1cb8dd63
ArthurJuwer <arthur.juwer99@gmail.com>
2024-08-20 19:35:10 -0300

    função de limite por tentativas adicionadaa!

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,15 +1,16 @@
 <?php
     require_once 'assets/php/conect.php';
     class Login{
-            protected $userEmail;
-            protected $userPassword; 
+            protected $usuarioEmail;
+            protected $usuarioSenha; 
             protected $emailSemelhantes ;
-            protected $passwordSemelhantes ;
-            protected $userType;
+            protected $senhaSemelante ;
+            protected $usuarioTipo;
             protected $ultimoLogin;
             protected $messageError;
             protected $showError;
             protected $mostrarAlerta;
+            protected $tentativas;
 
             public function __construct(){
                 $this->receberDadosFormulario();
@@ -33,9 +34,35 @@
             }
 
             private function receberDadosFormulario(){
-                $this->userEmail = $_POST['email'] ?? '';
-                $this->userPassword = $_POST['password'] ?? '';
+                $this->usuarioEmail = $_POST['email'] ?? '';
+                $this->usuarioSenha = $_POST['password'] ?? '';
             }
+
+            private function verificarTentativasUsuario(){
+                
+                if($this->tentativas >= 3){
+                    $this->mostrarErro();
+                    $this->messageError = 'Erro! Este email esta suspenso por enquanto.';
+                    $this->showError = 'on';
+                    
+                } else {
+                    $this->tentativas++;
+                    $this->atualizarTentativasUsuario();
+                }
+            }
+            private function atualizarTentativasUsuario(){
+                $pdo = conectar();
+
+                $TABELA = 'lista_usuarios';
+
+                $stmt = $pdo->prepare("UPDATE $TABELA SET attemptsEmail = :attemptsEmail WHERE email = :email");
+
+                $stmt->bindParam(':attemptsEmail', $this->tentativas);
+                $stmt->bindParam(':email', $this->usuarioEmail);
+
+                $stmt->execute();
+            }
+
             private function receberDadosBancoDados(){
                 $pdo = conectar();
 
@@ -43,32 +70,36 @@
 
                 $stmt = $pdo->prepare("SELECT * FROM $TABELA WHERE email LIKE :email");
 
-                $stmt->execute( array ( 'email' => '%' . $this->userEmail . '%' ) );
+                $stmt->execute( array ( 'email' => '%' . $this->usuarioEmail . '%' ) );
 
                 $dados = $stmt->fetch();
 
                 $this->emailSemelhantes = $dados['email'] ?? 'valor invalido';
-                $this->passwordSemelhantes = $dados['password'] ?? 'valor invalido';
-                $this->userType = $dados['user_type'] ?? 'valor invalido';
+                $this->senhaSemelante = $dados['password'] ?? 'valor invalido';
+                $this->tentativas = $dados['attemptsEmail'] ?? 'valor invalido';
+                $this->usuarioTipo = $dados['user_type'] ?? 'valor invalido';
 
             }
             public function verificarSemelhancasDados(){
-                $valoresIguaisDataBaseAndForm = ($this->userEmail == $this->emailSemelhantes && $this->userPassword == $this->passwordSemelhantes);
-        
-                if($valoresIguaisDataBaseAndForm) {   
+                $valoresIguaisBancoDadosFormulario = ($this->usuarioEmail == $this->emailSemelhantes && $this->usuarioSenha == $this->senhaSemelante);
+
+                $usuarioNaoEstaSuspenso = ($this->tentativas < 3);
+
+                if($valoresIguaisBancoDadosFormulario && $usuarioNaoEstaSuspenso) {   
                     $this->redirecionarDeAcordoComTipoUsuario();
                 } else {
                     $this->mostrarErro();
                     $this->messageError = 'Erro! Verfique se os dados estão certos.';
                     $this->showError = 'on';
+                    $this->verificarTentativasUsuario();
                 }
             }
             protected function redirecionarDeAcordoComTipoUsuario(){
                 session_start();
 
-                $_SESSION['email_usuario'] = $this->userEmail;
+                $_SESSION['email_usuario'] = $this->usuarioEmail;
                 $this->registrarLogin();
-                if($this->userType == 'User'){
+                if($this->usuarioTipo == 'User'){
                     header('Location: views/user/user_dashboard.php');
                 } else {
                     header('Location: views/admin/admin_dashboard.php');
@@ -79,7 +110,7 @@
                 $TABELA = 'lista_usuarios';
 
                 $stmt = $pdo->prepare("UPDATE $TABELA SET `lastLogin` = NOW() WHERE email = :email");
-                $stmt->execute(['email' => $this->userEmail]);
+                $stmt->execute(['email' => $this->usuarioEmail]);
             }
             public function mostrarErro(){
                 $showPopUp = [$this->showError, $this->messageError];
